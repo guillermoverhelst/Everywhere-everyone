ca46910b1c5ddc2494dcd35ef377011eefed3219
juan45202 <jjrm.son8@gmail.com>
2024-08-16 23:25:50 -0500

    Missing files

---
+++ src/app/pages/flights/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ListFlights() {
+    return (
+      <>
+        <h1 className="mt-5 text-center text-2xl font-bold">List Flights</h1>
+        <div className="w-full text-center"> contenedor de vuelos </div>
+      </>
+    );
+  }
